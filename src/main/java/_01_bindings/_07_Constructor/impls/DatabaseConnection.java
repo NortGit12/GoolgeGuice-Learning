58ef14f22e69703d636d5b08a2143a9727c1f4f8
Jeff Norton <jnorton@ancestry.com>
2012-10-30 11:21:35 -0600

    Added support for constructor bindings

---
+++ src/main/java/_01_bindings/_07_Constructor/impls/DatabaseConnection.java
@@ -0,0 +1,9 @@
+package _01_bindings._07_Constructor.impls;
+
+public class DatabaseConnection {
+  
+  public DatabaseConnection() {
+    System.out.println("Creating an instance of DatabaseConnection.");
+  }
+  
+}
